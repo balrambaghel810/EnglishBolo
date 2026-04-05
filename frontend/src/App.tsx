f7041cf5cabc63d20541b894489a596032377fdd
balrambaghel810 <131375971+balrambaghel810@users.noreply.github.com>
2026-04-05 16:12:14 +0530

    added logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -406,7 +406,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>EnglishBolo</h1>
+        <h1>English Bolo</h1>
         <p>Video Speech-to-Text with Grammar Correction</p>
       </header>
 
